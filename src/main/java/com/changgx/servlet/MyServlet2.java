d594feffb1dcb0dcd11707b02da3251808d0d394
Liu Jian <liujian20140108@gmail.com>
2019-04-23 15:38:09 +0800

    更新跳转页面代码

---
+++ src/main/java/com/changgx/servlet/MyServlet2.java
@@ -0,0 +1,36 @@
+package com.changgx.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *  @WebServlet注解需要使用servlet-api 比较新的包，老包不支持该注解
+ *
+ */
+@WebServlet("/servlet2")
+public class MyServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("/index2.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,4 +3,5 @@
 <h2>Hello World!</h2>
 </body>
 <a href="/servlet1">servlet1</a>
+<a href="/servlet2">servlet2</a>
 </html>
